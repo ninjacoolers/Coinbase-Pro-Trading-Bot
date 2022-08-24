3125d2d81307aca096827806837963bb485ac7de
ExpertBC <top1successfulman@gmail.com>
2022-08-24 16:05:23 +1000

    update

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 /* eslint-disable no-unused-vars */
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const config = require("./src/config");
+const pairs = require("./src/config/pairs.json");
+
+
 const pino = require("pino");
 const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+const app = express();
+
+var corsOptions = {
+  origin: "*"
+};
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return;
+  }
+  // Pass to next layer of middleware
+  next();
+});
+
 
 /*
 *   This is the entry point of program. Select the strategy or analyzer(s)
@@ -47,3 +87,16 @@ if (strategy == 'stoploss') {
     logger.debug(`Starting stop loss strategy`);
     momentumWithStopLossStrategyStart();
 }
+
+
+app.get("/api/balance/:network", async (req, res) => {
+    
+  
+})
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
